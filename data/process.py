a79f278a60699241bb30378f276859102999b64f
Jiao Jing Ping <elleryjiao@gmail.com>
2013-11-21 20:20:29 +0800

    modified file path

--- data/process.py
+++ data/process.py
@@ -4,10 +4,10 @@ import json
 from pprint import pprint
 import urllib.request
 
-in_filename = 'venues_orig.json'
-out_filename ='venues.json'
-prefix = 'venues/venue_'
-attribute = 'image'
+in_filename = 'original/artists_orig.json'
+out_filename ='artists.json'
+prefix = 'artists/artist_'
+attribute = 'poster'
 max = 30
 json_data=open(in_filename)
 list = json.load(json_data)
