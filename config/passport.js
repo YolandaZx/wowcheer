1506e6c5515f39d7d3e67493b1b0452f8fb238fb
JiaoJingPing <elleryjiao@gmail.com>
2013-11-22 17:50:43 +0800

    added ui for weibo qq login

--- config/passport.js
+++ config/passport.js
@@ -4,17 +4,29 @@
 
 var mongoose = require('mongoose')
 var LocalStrategy = require('passport-local').Strategy;
+var QQStrategy= require('passport-qq').Strategy;
+var WeiboStrategy = require('passport-weibo').Strategy;
 
 var localUserSchema = new mongoose.Schema({
 	email: String,
 	salt: String,
 	hash: String
 });
+
 var Users = mongoose.model('userauths', localUserSchema);
 
+var compilePath = function(provider, callbackPath) {
+	var patt=/:provider/g;
+	patt.compile(patt); 
+	str2=callbackPath.replace(patt,provider);	
+	return str2;
+}
 
-module.exports = function (passport, config) {
+module.exports = function (app,passport, config) {
 	var provider = config.provider;
+	var env = app.get('env');
+	var callback = provider.callback[env];
+	
 	// Local strategy
 	passport.use(new LocalStrategy(function(email, password,done){
 		Users.findOne({ email : email},function(err,user){
@@ -30,6 +42,37 @@ module.exports = function (passport, config) {
 			});
 		});
 	}));
+	
+	// Weibo strategy
+	var weiboCallback = compilePath("weibo",callback);
+	// Store the compiled callback into configuration
+	config.provider.weibo.callback = weiboCallback;
+	passport.use(new WeiboStrategy({
+		clientID: provider.weibo.id,
+		clientSecret: provider.weibo.secret,
+		callbackURL: weiboCallback
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+		User.findOrCreate({ weiboId: profile.id }, function (err, user) {
+		  return done(err, user);
+		});
+	  }
+	));
+
+	// QQ strategy
+	var qqCallback = compilePath("qq",callback);
+	config.provider.qq.callback = qqCallback;
+	passport.use(new QQStrategy({
+		clientID: provider.qq.id,
+		clientSecret: provider.qq.secret,
+		callbackURL: qqCallback
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+		User.findOrCreate({ qqId: profile.id }, function (err, user) {
+		  return done(err, user);
+		});
+	  }
+	));
 	
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
